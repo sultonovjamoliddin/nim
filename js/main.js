79b06181fc09a8f36f76ace59bb226aa0e3fc65c
sultonovjamoliddin <sultonovjamoliddin12@gmail.com>
2025-03-17 18:05:23 +0500

    “boldi”

--- js/main.js
+++ js/main.js
@@ -11,3 +11,15 @@ window.addEventListener("scroll", function () {
           navbar.classList.remove("navbar-shrink");
       }
   }
+
+  function openNavbar() {
+    document.getElementById("navbar-responsive").style.left = "0";
+    // document.getElementById("navbar-responsive").style.top = "0";
+  }
+  function closeNavbar() {
+    document.getElementById("navbar-responsive").style.left = "-100%";
+    // document.getElementById("navbar-responsive").style.top = "-100%";
+  }
+  
+  document.getElementById("navbar-open").addEventListener("click", openNavbar);
+  document.getElementById("navbar-close").addEventListener("click", closeNavbar);
